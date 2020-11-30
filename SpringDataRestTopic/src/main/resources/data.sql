109f6e2d65912cd2b5d33fd83763bce1b592acda
KETHIREDDY RAGHAVAREDDY <62951969+KRAGHAVAREDDY@users.noreply.github.com>
2020-12-01 03:20:20 +0530

    second commit

---
+++ SpringDataRestTopic/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values (101,'naveen','python');
+insert into alien values (102,'Raghava','java');
+insert into alien values (103,'kumar','sql');
+insert into alien values (104,'pradeep','studio');
+insert into alien values (105,'jack','angular');
+
